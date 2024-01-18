52b7e5b0394d61ab7f1ddfc421f0ab74519d265b
Bohdan Marko <goldenbohdanm@gmail.com>
2024-01-18 19:09:18 +0200

    Configre global API URLs with users app

--- library_service/urls.py
+++ library_service/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include("books_app.urls"))
+    path("api/", include("books_app.urls")),
+    path("api/users/", include("users.urls",)),
 ]
