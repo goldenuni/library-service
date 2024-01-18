c7cbd1cb4ad36a09611e6f482300eff39bafd1fc
Bohdan Marko <goldenbohdanm@gmail.com>
2024-01-18 19:04:49 +0200

    Add a missing TAB in class body

--- users/views.py
+++ users/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
