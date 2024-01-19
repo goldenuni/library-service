fab689ad1ad22b03db755a209da804fa1567a856
Bohdan Marko <goldenbohdanm@gmail.com>
2024-01-19 16:27:17 +0200

    Add a constraints for expected_return_date & actual_return_date

--- borrowings_app/models.py
+++ borrowings_app/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import MinValueValidator
 from django.db import models
 
 from books_app.models import Book
@@ -6,10 +7,16 @@ from books_app.models import Book
 
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
-    expected_return_date = models.DateField()
-    actual_return_date = models.DateField(null=True, blank=True)
+    expected_return_date = models.DateField(
+        validators=[MinValueValidator(borrow_date)]
+    )
+    actual_return_date = models.DateField(
+        null=True, blank=True, validators=[MinValueValidator(borrow_date)]
+    )
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.user.first_name} {self.user.last_name} borrowed {self.book.title}"
+        return (
+            f"{self.user.first_name} {self.user.last_name} borrowed {self.book.title}"
+        )
