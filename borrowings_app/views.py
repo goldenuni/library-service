8ef85bc6a179227c456232de58a52cd022f689cd
Bohdan Marko <goldenbohdanm@gmail.com>
2024-01-22 17:21:24 +0200

    Implemented List API View and Detail API View for Borrowing model
    Configure permissions, that only authenticated users can read info

--- borrowings_app/views.py
+++ borrowings_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.permissions import IsAuthenticated
+from borrowings_app.models import Borrowing
+from borrowings_app.serializers import BorrowingReadSerializer
 
-# Create your views here.
+
+class BorrowingListView(generics.ListAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingReadSerializer
+    permission_classes = [
+        IsAuthenticated,
+    ]
+
+
+class BorrowingDetailView(generics.RetrieveAPIView):
+    queryset = Borrowing.objects.all()
+    serializer_class = BorrowingReadSerializer
+    permission_classes = [
+        IsAuthenticated,
+    ]
