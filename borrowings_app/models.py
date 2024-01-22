27793b5f75f9f4838d4453c2deb2b4afb4529481
Bohdan Marko <goldenbohdanm@gmail.com>
2024-01-22 17:12:50 +0200

    Add a constraints for borrow_date, expected_return_date & actual_return_date

--- borrowings_app/models.py
+++ borrowings_app/models.py
@@ -1,11 +1,13 @@
+from datetime import date
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 from django.db import models
 
 from books_app.models import Book
 
 
 class Borrowing(models.Model):
-    borrow_date = models.DateField(auto_now_add=True)
+    borrow_date = models.DateField()
     expected_return_date = models.DateField()
     actual_return_date = models.DateField(
         null=True,
@@ -19,3 +21,26 @@ class Borrowing(models.Model):
             f"{self.user.first_name} {self.user.last_name} borrowed {self.book.title}"
         )
 
+    @staticmethod
+    def validate_borrowing(
+        borrow_date: date,
+        expected_return_date: date,
+        error_to_raise: Exception,
+        actual_return_date: date = None,
+    ):
+        if borrow_date <= expected_return_date:
+            if actual_return_date:
+                if actual_return_date < borrow_date:
+                    raise error_to_raise(
+                        "Actual return date must be greater or equal to borrow_date"
+                    )
+        else:
+            raise error_to_raise("Expected return date must be greater or equal to borrow_date")
+
+    def clean(self):
+        Borrowing.validate_borrowing(
+            self.borrow_date,
+            self.expected_return_date,
+            ValidationError,
+            self.actual_return_date,
+        )
