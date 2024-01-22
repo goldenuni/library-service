30165e45e3f77c5f20472e87bc99e2ff75acbcc3
Bohdan Marko <goldenbohdanm@gmail.com>
2024-01-22 16:38:07 +0200

    Remove MinValueValidator, return to version without constraints for dates

--- borrowings_app/models.py
+++ borrowings_app/models.py
@@ -1,5 +1,4 @@
 from django.contrib.auth import get_user_model
-from django.core.validators import MinValueValidator
 from django.db import models
 
 from books_app.models import Book
@@ -7,11 +6,10 @@ from books_app.models import Book
 
 class Borrowing(models.Model):
     borrow_date = models.DateField(auto_now_add=True)
-    expected_return_date = models.DateField(
-        validators=[MinValueValidator(borrow_date)]
-    )
+    expected_return_date = models.DateField()
     actual_return_date = models.DateField(
-        null=True, blank=True, validators=[MinValueValidator(borrow_date)]
+        null=True,
+        blank=True,
     )
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
@@ -20,3 +18,4 @@ class Borrowing(models.Model):
         return (
             f"{self.user.first_name} {self.user.last_name} borrowed {self.book.title}"
         )
+
