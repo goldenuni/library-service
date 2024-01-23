1c98b6d31a963606be8e754400054e849832e0d6
Bohdan Marko <goldenbohdanm@gmail.com>
2024-01-23 23:54:59 +0200

    Implement BorrowingReturnView
    Configure permissions, add a constraint: borrowing can be returned only once

--- borrowings_app/views.py
+++ borrowings_app/views.py
@@ -1,9 +1,15 @@
-from rest_framework import generics, permissions
+import datetime
+
+from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+
 from borrowings_app.models import Borrowing
 from borrowings_app.serializers import (
     BorrowingReadSerializer,
     BorrowingCreateSerializer,
+    BorrowingReturnSerializer,
 )
 
 
@@ -26,3 +32,45 @@ class BorrowingDetailView(generics.RetrieveAPIView):
     permission_classes = [
         IsAuthenticated,
     ]
+
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def borrowing_return_view(request, pk: int):
+    try:
+        instance = Borrowing.objects.get(pk=pk)
+    except Borrowing.DoesNotExist:
+        return Response(
+            {
+                "Error": "Not Found",
+            },
+            status=status.HTTP_404_NOT_FOUND,
+        )
+
+    if request.method == "POST":
+        if instance.is_active:
+
+            updated_data = {
+                "is_active": False,
+                "actual_return_date": datetime.date.today(),
+            }
+
+            serializer = BorrowingReturnSerializer(
+                instance,
+                data=updated_data,
+                partial=True,
+            )
+
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            else:
+                return Response(
+                    serializer.errors,
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+        else:
+            return Response(
+                {"Error": "You cannot return borrowing twice"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
