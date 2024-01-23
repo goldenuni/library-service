ed1d2505d0a1fbf00223021bb0820e1e450fdee7
Bohdan Marko <goldenbohdanm@gmail.com>
2024-01-23 23:55:47 +0200

    Add an URL for BorrowingReturn endpoint

--- borrowings_app/urls.py
+++ borrowings_app/urls.py
@@ -1,12 +1,18 @@
 from django.urls import path
-from borrowings_app.views import BorrowingListCreateView, BorrowingDetailView
-
+from borrowings_app.views import (
+    BorrowingListCreateView,
+    BorrowingDetailView,
+    borrowing_return_view,
+)
 
 app_name = "borrowings_app"
 
 urlpatterns = [
     path("", BorrowingListCreateView.as_view(), name="borrowing-list"),
+    path("<int:pk>/", BorrowingDetailView.as_view(), name="borrowing-detail"),
     path(
-        "<int:pk>/", BorrowingDetailView.as_view(), name="borrowing-detail"
+        "<int:pk>/return/",
+        borrowing_return_view,
+        name="borrowing-return",
     ),
 ]
