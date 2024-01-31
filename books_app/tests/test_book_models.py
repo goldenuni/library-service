3060f3c9a042b1d0e01ff0ac53c33acf02ce2f80
Bohdan Marko <goldenbohdanm@gmail.com>
2024-01-31 22:38:46 +0200

    Reformat the file, add TABs

--- books_app/tests/test_book_models.py
+++ books_app/tests/test_book_models.py
@@ -3,14 +3,13 @@ from books_app.models import Book
 
 
 class BookModelTest(TestCase):
-
     def setUp(self):
-      self.book = Book.objects.create(
+        self.book = Book.objects.create(
             title="To Kill a Mockingbird",
             author="Harper Lee",
             cover=Book.SOFT,
             inventory=50,
-            daily_fee=7.99
+            daily_fee=7.99,
         )
 
     def test_str_method(self):
