3f980c026530821f6205ad286779042f00774e80
Bohdan Marko <goldenbohdanm@gmail.com>
2024-01-31 23:19:31 +0200

    Refactor, add a blank line after imports

--- users/tests.py
+++ users/tests.py
@@ -1,13 +1,14 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class UserModelTest(TestCase):
     def setUp(self):
         self.user = get_user_model().objects.create(
             email="testovich@test.com",
             password="UltraSecretPassword304!",
             first_name="John",
-            last_name="Lennon"
+            last_name="Lennon",
         )
 
     def test_str_method(self):
